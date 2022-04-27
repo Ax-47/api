82402a03963e41974bc6fb82f235925e656cca6b
ax47 <85576471+Ax-47@users.noreply.github.com>
2022-04-27 22:27:08 +0700

    Add files via upload

--- apigo/main.go
+++ apigo/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	q "Axapi/question"
+	p "Axapi/path"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	r := gin.Default()
 	r.POST("/q", func(c *gin.Context) {
-		q.M(c)
+		p.M(c)
 	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.POST("/register", func(c *gin.Context) {
+		p.Register(c)
+	})
+
+	r.Run(":8080")
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	/*
 		#Example
 		import request
